24211d7110bac77a0d1e5458c849c47f333d79b0
limgunny <red373712345@gmail.com>
2024-04-19 21:47:17 +0900

    new

---
+++ main.js
@@ -0,0 +1,61 @@
+
+function showbanana(){
+    document.getElementById('fig').src ='img/banana1.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>열대지역에서 재배한 부드럽고 쫀득한 식감의 고당도 바나나<br>고당도 프리미엄 바나나<br>맛과 품질, 신선한 바나나를 원더프룻이 제안하는 합리적인 가격으로 만나보세요!'
+}
+
+function showapple(){
+    document.getElementById('fig').src ='img/apple3.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>400청정지역인 국내 최대 사과 산지<br>경북 안동에서 재배한 맛좋은 사과를<br>주문 해보세요'
+}
+
+function showpear(){
+    document.getElementById('fig').src ='img/pear.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>풍부한 과즙과 아삭한 식감이 매력적이에요.<br> 시원하면서도 달콤한 맛으로 남녀노소 즐기기 좋답니다.<br> 당도선별기와 다년간의 노하우로 달콤한 배만을 꼼꼼하게 선별, 포장했습니다.'
+}
+
+function showgrape(){
+    document.getElementById('fig').src ='img/grape.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>수입 포도의 대표 산지에서 자란 칠레산 카라샬 포도를 지금 만나보세요.<br> 계곡 지형의 높은 일교차에서 재배하고,<br> 일반 포도나무와 달리 전정을 많이 해 과실의 크기가 큰 고품질의 포도를 수확했습니다'
+}
+
+function showstrawberry(){
+    document.getElementById('fig').src ='img/strawberry.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></span></b><br>진한 달콤함과 아삭하고 산뜻한 식감이예요<br> 한입에 쏙 들어가는 크기의 당도높은 딸기예요'
+}
+
+function showwatermelon(){
+    document.getElementById('fig').src ='img/watermelon.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>계절과 상관없이 언제든 달콤한 수박을 드실 수 있도록 준비했습니다.<br> 당도 선별 과정을 거쳐 더욱 달콤한 수박이에요.<br> 아삭아삭한 과육과 시원한 맛이 매력적이랍니다.'
+}
+
+function showmango(){
+    document.getElementById('fig').src ='img/mango.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>달콤한 열대과일, 필리핀 망고를 만나보세요.<br> 풍부한 영양과 달콤한 맛으로 열대과일의 여왕이라고 불리는 과일이랍니다.<br> 망고는 껍질을 벗겨 디저트나 간식으로 즐기기 좋을 만큼 진하고 깊은 달콤함을 자랑해요.'
+}
+
+function showkiwi(){
+    document.getElementById('fig').src ='img/kiwi.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>부드러운 골드 색상의 과육을 지닌 유기농 제스프리 썬골드키위입니다.<br> 따스한 햇살 받고 유기농으로 자란 키위는 달콤한 맛과 새콤한 맛이 조화를 이루고 있어요.<br> 자꾸만 먹게 되는 키위 속 콕콕 박혀있는 검은 씨앗도 매력적인 식감을 지니고 있답니다. '
+}
+
+function showpeach(){
+    document.getElementById('fig').src ='img/plum.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>계절과 상관없이 언제든 달콤한 복숭아를 드실 수 있도록 준비했습니다.<br> 당도 선별 과정을 거쳐 더욱 달콤한 복숭아예요.<br> 아삭아삭한 과육과 시원한 맛이 매력적이랍니다.'
+}
+
+function showplum(){
+    document.getElementById('fig').src ='img/peach.png'
+    document.getElementById('desc').innerHTML=
+    '<span style=\"background: yellow; font-size: 30px; color: red;\"><b>4990원</span></b><br>한 해 수확되는 자두 중 마지막으로 수확하는 원더푸룻 자두<br>과즙이 많고 당도가 높으며, 말랑한 여름 자두와는 달리 과육이 단단해<br> 아삭한 식감으로 즐길 수 있는 가을 자두를 지금 만나보세요'
+}
+
